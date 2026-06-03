4e0645a522dab6e9c8a26fea0b0ed4319aba46eb
Lenin Valverde <leninvalve@gmail.com>
2026-06-02 23:31:45 -0500

    Used shellcheck and fixed errors

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 #####################################
 # Constants
 #####################################
@@ -5,12 +8,13 @@ readonly JOBS_IN_DIR="./jobs_in" 				# Jobs-in dir (written by PHP).
 readonly JOBS_OUT_DIR="./jobs_out" 			# Jobs-out dir (written by Bash).
 readonly LOGS_DIR="/var/log/provision"
 readonly LOGS_FILE="main.log"
+readonly QTY_ALLOWED_FIELDS=3
 
 log() {
 	local level="${1:-info}"
 	local msg="${2:-}"
-	local datetime=$(date "+%Y-%m-%d %H:%M:%S")
-	local prefix
+	local datetime prefix
+	datetime=$(date "+%Y-%m-%d %H:%M:%S")
 
 	[ -z "$msg" ] && return 1
 
@@ -20,38 +24,17 @@ log() {
 		*)		prefix="INFO:" ;;
 	esac	
 
-	if [ ! -r "$LOGS_DIR/$LOGS_FILE" ]; then
-		mkdir -p "$LOGS_DIR"
-		chmod 700 "$LOGS_DIR"
-		touch "$LOGS_DIR/$LOGS_FILE"
-		chmod 600 "$LOGS_DIR/$LOGS_FILE"
+	if [ ! -r "${LOGS_DIR}/${LOGS_FILE}" ]; then
+		mkdir -p "${LOGS_DIR}"
+		chmod 700 "${LOGS_DIR}"
+		touch "${LOGS_DIR}/${LOGS_FILE}"
+		chmod 600 "${LOGS_DIR}/${LOGS_FILE}"
 	fi
-	printf '[ %s ] %s %s\n' "$datetime" "$prefix" "$msg" >> "$LOGS_DIR/$LOGS_FILE"
+	printf '[ %s ] %s %s\n' "$datetime" "$prefix" "$msg" >> "${LOGS_DIR}/${LOGS_FILE}"
 }
 
-# Make sure the quantity and name of
-# keys received from PHP are correct.
-# No extra/missing keys.
-# Quantity of keys is also checked.
-# Not empty/null values also checked.
 is_format_ok() {
 	local job_file="$1"
-	# From bash array to json so jq manages it better.
-	local bash2json=$(printf '%s\n' "${JSON_KEYS[@]}" | jq -R. | jq -s -c.)
-
-	jq -e --argjson need "$bash2json" '
-		(keys | sort) == ($need | sort) and
-		all(.[];.!= null and.!= "")
-	' "$job_file" > /dev/null	
-}
-
-# ============================================
-# Validate input. Bash is the last seatbelt.
-# Remember to check the constant holding
-# the json keys for naming vars.
-# ============================================
-are_json_values_ok() {
-    [[ $user_email =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]] || return 1
-    [[ $user_names =~ ^[A-Za-z0-9\ _-]{1,64}$ ]] || return 1
-    [[ $user_subdomain =~ ^[a-z0-9-]{1,63}$ ]] || return 1
+	[ $(wc -l < "$job_file") -eq "$QTY_ALLOWED_FIELDS" ] || return 1
+	return 0
 }
--- provision.sh
+++ provision.sh
@@ -6,33 +6,27 @@ if [ ! "$(id -u)" -eq 0 ]; then
 	exit 1
 fi
 
-# Try to load library file with constants and functions.
 readonly HELPERS_LIB="./lib/helpers.sh"
 if [ ! -r "$HELPERS_LIB" ]; then
 	echo "Unable to load '$HELPERS_LIB'. Quitting." >&2
 	exit 1
 fi
 # Library file exists and is readable. Load it.
+# shellcheck source=lib/helpers.sh
 source "$HELPERS_LIB"
 
 process_job() {
-	# Full path to json job file. Example: /path/to/job/file/mdk3KdAeUd
-	local job_file="${1:-}"
-	# Job file name only. Example: sjAjda8Ja
-	local job_id=$(basename "$job_file")
+	local job_id
+	local job_file="$1" 						# Example: /full/path/to/job/file/mdk3KdAeUd
+	job_id=$(basename "$job_file")	# Example: mdk3KdAeUd
 
 	if ! is_format_ok "$job_file"; then
-		log "err" "Job file badly formatted. Quitting."
+		log "err" "Job file '$job_id' badly formatted. Quitting."
 		# TODO: Write to the output file so that user with this specific job
 		# knows something went wrong.
 		return 1
 	fi
-
-	if ! are_json_values_ok; then
-		log "err" "Invalid provided value(s). Quitting."
-		# TODO: Write to the output file...
-		return 1
-	fi
+	log ok "'${job_id}' has exactly ${QTY_ALLOWED_FIELDS} fields."
 }
 
-process_job "dummy_input"
+process_job "${JOBS_IN_DIR}/dummy_job"
